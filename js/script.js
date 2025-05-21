ee496f69567895f5db3bf189b5a3ddc159bb2e41
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-21 17:21:08 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -10,16 +10,20 @@
 async function myButtonClicked() {
 
   try {
-     // defining variables
-  const currentWeather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-  const jsonData = await currentWeather.json()
-  const weather = jsonData.main.temp 
-  const jsonweather =  jsonData.weather[0].description
+    // defining variables
+    const currentWeather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+    const jsonData = await currentWeather.json()
+    const weatherInKelvin = jsonData.main.temp 
+    const jsonWeatherDescription = jsonData.weather[0].description
+    const iconCode = jsonData.weather[0].icon
+    const iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+    const weatherInCelsius = Math.round(weatherInKelvin - 273.15)
 
-  // output
-  document.getElementById("answer").innerHTML = "<p>The current temperature is " + jsonweather + "°C," + weather + "</p>"
+    // output
+    document.getElementById("answer").innerHTML ="<p>The current temperature is " + weatherInCelsius + " °C, " + jsonWeatherDescription + "</p>" +
+    "<img src='" + iconURL + "' alt='Weather icon'>"
   } catch (error) {
     console.error(error)
+    document.getElementById("answer").innerHTML = "<h4>Error 404</h4>"
   }
 }
-
